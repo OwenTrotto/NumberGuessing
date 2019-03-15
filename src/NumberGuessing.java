614ce889ccadc6e23814e786942ba1f254c4bfef
Owen <owen.trotto.2021@student.lhschools.org>
2019-03-15 08:15:10 -0400

    Started Number Guess Game

---
+++ src/NumberGuessing.java
@@ -0,0 +1,14 @@
+public class NumberGuessing {
+
+
+    public static void main(String[]args) {
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/src/org/LickingHeights/Main.java
@@ -0,0 +1,22 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+        int pickedNumber;
+
+        pickedNumber = (int)(Math.random()*100);
+
+        System.out.println(pickedNumber);
+
+
+
+
+
+
+    }
+}
